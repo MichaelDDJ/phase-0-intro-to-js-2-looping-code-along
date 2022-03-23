0e452d606cba956e3f6dd636855f5990eb8a697b
Michael Davis <jrmdd1496@gmail.com>
2022-03-23 11:11:16 -0500

    commit tree 2178f3ef4c8933184296e147602c8e663dd5e300

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let newArr = [];
+let arr = ['a', 'b', 'c'];
+
+function writeCards(names, event) { 
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(x) {
+    for (let i = x; i >= 0; i--) {
+        console.log(x);
+        x--;
+    }
+}
